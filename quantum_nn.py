8ae7df586369e82cf4b61c3bdf3c21cfe20e07af
Pedro Garzon <pedropgarzon@gmail.com>
2019-05-28 23:04:42 -0700

    have a compiling circuit going

--- quantum_nn.py
+++ quantum_nn.py
@@ -14,7 +14,7 @@ import random
 
 ##############################################################
 
-test_dev = qml.device('forest.qvm', device='10q-pyqvm', noise=False, shots=1000)
+test_dev = qml.device('forest.qvm', device='11q-pyqvm', noise=False, shots=1000)
 
 
 
@@ -47,10 +47,35 @@ def hadamard_list(qbit_list):
     for qbit in qbit_list:
         qml.Hadamard(qbit)
 
+@qml.qnode(test_dev)
+def reset_circuit():
+
+    # for i in reversed(range(8,1)):
+    #     qml.PauliX(wires=i)
+    #
+    # for i in reversed(range(8,1)):
+    #     qml.expval.PauliZ(wires=i)
+
+    qml.PauliX(wires=0)
+    return qml.expval.PauliX(wires=0)
+
+
+def correct_to_ground(ground_truth):
+    if int(round(ground_truth)):
+        qml.PauliX(wires=0)
+
+
+def flip_startbit(qubit):
+    print("flipping the bit")
+    qml.PauliX(wires=qubit)
+
+
 
 # Declare quantum circuit
-@qml.qnode(test_dev)
-def shor_encoding(input_wire):
+# @qml.qnode(test_dev)
+def shor_encoding():
+    # Always have the 0 bit be the start bit
+    # Measure it so it's set to 0
 
     first_level_qubits = (0, 3, 6)
     bit_encode(*first_level_qubits)
@@ -64,35 +89,52 @@ def shor_encoding(input_wire):
     # Simulate single qubit errors
     add_single_qubit_error()
 
-    expectation_list = []
 
+    # expectation_list = []
     # return qml.expval.PauliZ(wires=0)
     #
-    for qbit in range(9):
-        expecatation_val = qml.expval.PauliZ(wires=qbit)
-        expectation_list.append(expecatation_val)
-
-    return expectation_list
+    # for qbit in range(9):
+    #     expecatation_val = qml.expval.PauliZ(wires=qbit)
+    #     expectation_list.append(expecatation_val)
+    #
+    # return expectation_list
 
 
 def shor_decoding_model(weights):
     # Our neural network
+    output_wire = 9
     for i in range(9):
         first = i
-        second = 9
         qml.RX(weights[0], wires=first)
         qml.RY(weights[2], wires=first)
         qml.RZ(weights[4], wires=first)
 
-        qml.CNOT(wires=[first, second])
+        qml.CNOT(wires=[first, output_wire])
 
-        qml.RX(weights[1], wires=second)
-        qml.RY(weights[3], wires=second)
-        qml.RZ(weights[5], wires=second)
+        qml.RX(weights[1], wires=output_wire)
+        qml.RY(weights[3], wires=output_wire)
+        qml.RZ(weights[5], wires=output_wire)
 
 
 @qml.qnode(test_dev)
-def shor_decoding_circuit(weights):
+def shor_decoding_circuit(weights, ground_truth):
+
+    # Flip so the qbit is set to ground truth
+    # correct_to_ground(ground_truth)
+
+
+    # With random probability start with 1 as the ground truth bit
+    # if random.random() < 0.5:
+    #     flip_startbit(0)
+    #     ground_truth = 1
+
+
+
+
+    shor_encoding()
+
+
+
     # We need to wrap ground_truth_qbit_value in a bigger function, like the pennylane notebook does.
     # The API for pennylane doesn't let us pass in any more parameters
     # So we can then pass it into the decoding circuit
@@ -102,32 +144,61 @@ def shor_decoding_circuit(weights):
 
     # Assume the decoding circuit has been run, and the output is on wire 9
     #  Create ground truth qubit on wire 0
-    qml.reset(wires=0)  # This is PSEUDOCODE RN
-    if ground_truth_qbit_value == 1:
-        qml.X(0)
+
+    # This is PSEUDOCODE RN
+    # qml.reset(wires=0)
+
+
+    # TODO: To reset wire 0 just measure it to fix it
+    # qml.expval.PauliZ(wires=10)
+
+
+
+    if ground_truth == 1:
+        qml.X(10)
 
     #entangle and measure the parity
-    qml.CNOT(0, 9)
+    qml.CNOT(wires=[10, 9])
     return qml.expval.PauliZ(wires=9)
 
 
+
+
 def loss_function(weights):
-    measurement = shor_decoding_circuit(weights)
+    # Have shor_encoding prepare and also give back the true value of the bit
+
+    # Get a blank slate for the working Shor bits
+    ground_truth = reset_circuit()
+
+
+    print("GROUND TRUTH :%f" % ground_truth)
+    # now round ground truth
+    ground_truth = int(round(ground_truth))
+
+
+
+    print("entering shor decoding step")
+    measurement = shor_decoding_circuit(weights, ground_truth)
+
     return -(measurement + 1) / 2
 
 
-def train(model_function, weights):
+def train(weights):
     # A training loop. Use GDO?
     # Construct our CNOt loss
     alpha = 0.1
     optimizer = GradientDescentOptimizer(alpha)
 
+
+
     # Optimize D, fix G
     for it in range(50):
         disc_weights = optimizer.step(loss_function, weights)
-        cost = optimizer(disc_weights)
+        cost = loss_function(disc_weights)
         # if it % 1 == 0:
         print("Step {}: cost = {}".format(it + 1, cost))
+        print("END STEP\n\n\n")
+
 
 
 
@@ -136,7 +207,14 @@ def train(model_function, weights):
 
 
 if __name__ == "__main__":
-    output = shor_encoding(0)
-    print(output)
+    eps = 1e-2
+    weights = np.array([np.pi] + [0] * 5) + np.random.normal(scale=eps, size=[6])
+    print("weights before")
+    print(weights)
+
+    train(weights)
+
+    print("weights after")
+    print(weights)
 
     # Other things if necessary
